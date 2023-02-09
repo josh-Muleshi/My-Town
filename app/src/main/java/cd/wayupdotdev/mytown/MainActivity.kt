5112059006cf8e6586a90f0b912533738a85e19e
Josh Muleshi <jmuleshi2@gmail.com>
2023-02-09 09:11:50 +0200

    app files

--- app/src/main/java/cd/wayupdotdev/mytown/MainActivity.kt
+++ app/src/main/java/cd/wayupdotdev/mytown/MainActivity.kt
@@ -11,33 +11,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import cd.wayupdotdev.mytown.ui.theme.MyTownTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyTownTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyTownTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/cd/wayupdotdev/mytown/app/App.kt
@@ -0,0 +1,7 @@
+package cd.wayupdotdev.mytown.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application()
