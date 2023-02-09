ec7bfdf7c068f7db84ab897bbf06087070d5ee36
Josh Muleshi <jmuleshi2@gmail.com>
2023-02-09 10:02:34 +0200

    setup root

--- app/src/main/java/cd/wayupdotdev/mytown/MainActivity.kt
+++ app/src/main/java/cd/wayupdotdev/mytown/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import cd.wayupdotdev.mytown.navigation.SetupNavGraph
 import cd.wayupdotdev.mytown.ui.theme.MyTownTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    SetupNavGraph()
                 }
             }
         }
--- app/src/main/java/cd/wayupdotdev/mytown/presentation/Screen/profile/view/ProfileScreen.kt
+++ app/src/main/java/cd/wayupdotdev/mytown/presentation/Screen/profile/view/ProfileScreen.kt
@@ -1,7 +1,9 @@
 package cd.wayupdotdev.mytown.presentation.Screen.profile.view
 
 import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
 
+@Destination
 @Composable
 fun ProfileScreen() {
 
